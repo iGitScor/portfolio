a05986e9da3cecd62b041f318590a1b0d1d32f59
iGitScor <sebastien.correaud@gmail.com>
2014-10-02 15:47:02 +0000

    Optimization website

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.configure(function() {
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(app.router);
+  app.use(express.compress());
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
 
