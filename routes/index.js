12c7dcc76f69fbd1c5d5d65dbe92e8d301421362
iGitScor <sebastien.correaud@gmail.com>
2014-10-06 11:55:36 +0000

    Optimize scripts minifier

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,29 @@
  */
 
 var compressor = require('node-minify'),
-fs = require("fs");
+    fs = require("fs");
 
-exports.index = function(req, res){
-    if (!fs.existsSync('public/js/dist/index-min-gcc.js')) {
-        fs.writeFileSync('public/js/dist/index-min-gcc.js', '');
+exports.index = function(req, res) {
+    var compiledJSPath = '/js/dist-index-min-gcc.js',
+        compiledCSSPath = '/css/dist-index-min-yui.css';
+
+    if (!fs.existsSync('public' + compiledJSPath)) {
+        fs.writeFileSync('public' + compiledJSPath, '');
     }
-    
-    fs.stat('public/js/dist/index-min-gcc.js', function(err, stats) {
+
+    fs.stat('public' + compiledJSPath, function(err, stats) {
         var dInterval;
         if (stats.size > 0) {
             var now = new Date();
             var mTime = new Date(stats.mtime);
-        
+
             var interval = Math.abs(now.getTime() - mTime.getTime());
             dInterval = interval / (1000 * 3600 * 24);
-        } else {
+        }
+        else {
             dInterval = -1;
         }
-        
+
         // One week cache
         if (dInterval > 7 || dInterval == -1) {
             // Using Google Closure
@@ -32,30 +36,31 @@ exports.index = function(req, res){
                 type: 'gcc',
                 publicFolder: 'public/js/',
                 fileIn: ['external/jquery-2.1.0.min.js', 'session-1.0.min.js', 'external/owl.carousel.min.js', 'animation-1.0.min.js'],
-                fileOut: 'public/js/dist/index-min-gcc.js',
-                callback: function(err, min){
+                fileOut: 'public' + compiledJSPath,
+                callback: function(err, min) {
                     console.log(err);
                 }
             });
         }
     });
-    
-    if (!fs.existsSync('public/css/index-min-yui.css')) {
-        fs.writeFileSync('public/css/index-min-yui.css', '');
+
+    if (!fs.existsSync('public' + compiledCSSPath)) {
+        fs.writeFileSync('public' + compiledCSSPath, '');
     }
-    
-    fs.stat('public/css/index-min-yui.css', function(err, stats) {
+
+    fs.stat('public' + compiledCSSPath, function(err, stats) {
         var dInterval;
         if (stats.size > 0) {
             var now = new Date();
             var mTime = new Date(stats.mtime);
-        
+
             var interval = Math.abs(now.getTime() - mTime.getTime());
             dInterval = interval / (1000 * 3600 * 24);
-        } else {
+        }
+        else {
             dInterval = -1;
         }
-        
+
         // One week cache
         if (dInterval > 7 || dInterval == -1) {
             // Using YUI Compressor for CSS
@@ -63,21 +68,20 @@ exports.index = function(req, res){
                 type: 'yui-css',
                 publicFolder: 'public/css/',
                 fileIn: ['style-1.0.min.css', 'layout-1.0.min.css', 'hint.css', 'badge-1.0.min.css', 'pages/cv.presentation.css', 'external/owl.carousel.css', 'animation-1.0.min.css', 'sprite.flags-1.0.min.css'],
-                fileOut: 'public/css/index-min-yui.css',
-                callback: function(err, min){
+                fileOut: 'public' + compiledCSSPath,
+                callback: function(err, min) {
                     console.log(err);
                 }
             });
         }
     });
-    
+
     res.render(
-        'index', 
-        { 
-            title: 'Curriculum vitae', 
+        'index', {
+            title: 'Curriculum vitae',
             message: req.flash('information'),
-            scripts: '/js/dist/index-min-gcc.js',
-            styles: '/css/index-min-yui.css'
+            scripts: compiledJSPath,
+            styles: compiledCSSPath
         }
     );
 };
