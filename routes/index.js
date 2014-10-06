81678b1e5cb94adf92463ab38a557b1667c847be
iGitScor <sebastien.correaud@gmail.com>
2014-10-06 11:40:40 +0000

    Optimize scripts minifier

--- routes/index.js
+++ routes/index.js
@@ -40,12 +40,44 @@ exports.index = function(req, res){
         }
     });
     
+    if (!fs.existsSync('public/css/dist/index-min-yui.css')) {
+        fs.writeFileSync('public/css/dist/index-min-yui.css', '');
+    }
+    
+    fs.stat('public/css/dist/index-min-yui.css', function(err, stats) {
+        var dInterval;
+        if (stats.size > 0) {
+            var now = new Date();
+            var mTime = new Date(stats.mtime);
+        
+            var interval = Math.abs(now.getTime() - mTime.getTime());
+            dInterval = interval / (1000 * 3600 * 24);
+        } else {
+            dInterval = -1;
+        }
+        
+        // One week cache
+        if (dInterval > 7 || dInterval == -1) {
+            // Using YUI Compressor for CSS
+            new compressor.minify({
+                type: 'yui-css',
+                publicFolder: 'public/css/',
+                fileIn: ['style-1.0.min.css', 'layout-1.0.min.css', 'hint.css', 'badge-1.0.min.css', 'pages/cv.presentation.css', 'external/owl.carousel.css', 'animation-1.0.min.css', 'sprite.flags-1.0.min.css'],
+                fileOut: 'public/css/dist/index-min-yui.css',
+                callback: function(err, min){
+                    console.log(err);
+                }
+            });
+        }
+    });
+    
     res.render(
         'index', 
         { 
             title: 'Curriculum vitae', 
             message: req.flash('information'),
-            scripts: '/js/dist/index-min-gcc.js'
+            scripts: '/js/dist/index-min-gcc.js',
+            styles: '/css/dist/index-min-yui.css'
         }
     );
 };
