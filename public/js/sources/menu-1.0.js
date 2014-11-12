f0421b675fe7d3222a7cf1d54c8a54de3e72bd28
iGitScor <sebastien.correaud@gmail.com>
2014-11-12 15:58:12 +0000

    Anchor click handler

--- public/js/sources/menu-1.0.js
+++ public/js/sources/menu-1.0.js
@@ -5,3 +5,17 @@ $("#mobile-navigation").each(function() {
         return location.href = h.options[h.selectedIndex].value;
     });
 });
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
